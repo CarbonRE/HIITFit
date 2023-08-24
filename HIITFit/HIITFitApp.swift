7d21ed8cbc4979fb4c0ffe8b47971139c56f8678
CarbonRE <carbonremn@gmail.com>
2023-08-24 18:18:26 +0200

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Vera Remn on 24/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
